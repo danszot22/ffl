7fcbed59e8cacbe0a07d8bf7b4d9214d161ec321
Dan Szot <danszot@hotmail.com>
2023-11-28 08:14:55 -0500

    Fix table header.

--- src/components/Scoring/CategoryPlayerList.js
+++ src/components/Scoring/CategoryPlayerList.js
@@ -1,10 +1,8 @@
-import { Paper, Table, TableBody, TableCell, TableHead, TableRow, Typography, useMediaQuery, useTheme } from "@mui/material";
+import { Paper, Table, TableBody, TableCell, TableHead, TableRow, Typography } from "@mui/material";
 import CategoryPlayerRow from './CategoryPlayerRow';
 import { grey } from "@mui/material/colors";
 
 export default function CategoryPlayerList({ players, showProjections, title, showGame }) {
-    const theme = useTheme();
-    const isAboveSmall = useMediaQuery(theme.breakpoints.up('sm'))
 
     return (
         <Paper elevation={3} >
@@ -12,9 +10,6 @@ export default function CategoryPlayerList({ players, showProjections, title, sh
                 <TableHead>
                     <TableRow>
                         <TableCell>{title}</TableCell>
-                        {showGame && isAboveSmall ? (
-                            <TableCell>Game</TableCell>
-                        ) : null}
                         <TableCell align="right">Total</TableCell>
                         {showProjections ? (
                             <TableCell align="right">Projected</TableCell>
